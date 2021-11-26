e6ea1187d9f6dbe07be88ad443460c3ee0e2470c
AjayNavgire <ajaynawgire@live.com>
2021-11-26 07:36:03 +0530

    feat: front end changes

--- src/App.tsx
+++ src/App.tsx
@@ -3,45 +3,35 @@ import { Switch, Route, Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
-import AddTutorial from "./components/add-tutorial.component";
-import Tutorial from "./components/tutorial.component";
-import TutorialsList from "./components/tutorials-list.component";
 import AddSchool from "./components/add-school.component";
 import AddTeacher from "./components/add-teacher.component";
 import AddStudent from "./components/add-student.component";
+import SchoolList from "./components/school-list.component";
 
 class App extends Component {
   render() {
     return (
       <div>
         <nav className="navbar navbar-expand navbar-dark bg-dark">
-          <Link to={"/tutorials"} className="navbar-brand">
-            bezKoder
+          <Link to={"/schools"} className="navbar-brand">
+            School App
           </Link>
           <div className="navbar-nav mr-auto">
-            <li className="nav-item">
-              <Link to={"/tutorials"} className="nav-link">
-                Tutorials
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link to={"/add"} className="nav-link">
-                Add
-              </Link>
-            </li>
+            
+           
             <li className="nav-item">
               <Link to={"/addSchool"} className="nav-link">
-                Add School
+                 School
               </Link>
             </li>
             <li className="nav-item">
               <Link to={"/addTeacher"} className="nav-link">
-                Add Teacher
+                 Teacher
               </Link>
             </li>
             <li className="nav-item">
               <Link to={"/addStudent"} className="nav-link">
-                Add Student
+                 Student
               </Link>
             </li>
           </div>
@@ -49,12 +39,11 @@ class App extends Component {
 
         <div className="container mt-3">
           <Switch>
-            <Route exact path={["/", "/tutorials"]} component={TutorialsList} />
-            <Route exact path="/add" component={AddTutorial} />
-            <Route path="/tutorials/:id" component={Tutorial} />
+            <Route exact path={["/", "/addSchool"]} component={AddSchool} />
             <Route path="/addSchool" component={AddSchool} />
             <Route path="/addTeacher" component={AddTeacher} />
             <Route path="/addStudent" component={AddStudent} />
+            <Route path= "/schools" component={SchoolList}/>
           </Switch>
         </div>
       </div>
---
+++ src/components/school-list.component.tsx
@@ -0,0 +1,17 @@
+import { Component } from "react";
+type Props = {};
+type State = {
+}
+
+export default class SchoolList extends Component<Props, State>{
+    constructor(props: Props) {
+        super(props)
+    }
+ render(){
+     return(
+         <div>
+            Hellow
+         </div>
+     )
+ }
+}
